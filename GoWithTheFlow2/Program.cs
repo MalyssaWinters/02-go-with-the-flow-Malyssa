2554ec14d7726b62fb00d6f1d2160c27648b434e
Malyssa Winters <malyssawinters5@gmail.com>
2016-06-16 17:00:20 -0400

    flow chart

---
+++ GoWithTheFlow2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoWithTheFlow2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Flow Chart of a Cat Lady");
+            Console.WriteLine("Are you happy?");
+
+            string answer = Console.ReadLine();
+                        
+            if (answer.ToLower() == "yes")
+            {
+                Console.WriteLine("End");
+                answer = Console.ReadLine();
+
+            }
+
+            else
+            {
+                Console.WriteLine("Get another cat");
+
+            }
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
